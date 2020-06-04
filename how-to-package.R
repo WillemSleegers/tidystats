de778d9f872355a2d6915ecbc93b50b6be06d1f7
Willem Sleegers <w.sleegers@me.com>
2020-06-05 01:17:19 +0200

    Update pkgdown website.

--- how-to-package.R
+++ how-to-package.R
@@ -47,4 +47,8 @@ usethis::use_vignette("introduction-to-tidystats")
 
 # Build website -----------------------------------------------------------
 
+# Run once to configure package to use pkgdown
+# usethis::use_pkgdown()
+
+# Run to build the website
 pkgdown::build_site()
