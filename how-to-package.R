688f90b00ebd2f831742f0e5fb51c52673a05759
willemsleegers <w.sleegers@me.com>
2022-03-13 20:24:13 +0100

    Update README and pkgdown website

--- how-to-package.R
+++ how-to-package.R
@@ -41,7 +41,7 @@ testthat::test_file("tests/testthat/test_htest.R")
 # Build website -----------------------------------------------------------
 
 # Run once to configure package to use pkgdown
-# usethis::use_pkgdown()
+usethis::use_pkgdown()
 
 # Run to build the website
 pkgdown::build_site()
