c79fd13a36acc11a561aa6a8a0d56ea2cb2a5cf6
willemsleegers <w.sleegers@me.com>
2023-01-24 22:12:38 +0100

    Added support for irr's ICC

--- R/tidy_stats.irr.r
+++ R/tidy_stats.irr.r
@@ -1,35 +1,32 @@
 #' @describeIn tidy_stats tidy_stats method for class 'icclist'
 #' @export
 tidy_stats.icclist <- function(x, args = NULL) {
-  # Create the analysis list and set the method
   analysis <- list(method = "ICC")
   
-  # Extract statistics
-  statistics <- list()
+  statistics <- list() %>%
+    add_statistic("N subjects", x$subjects, "N", "subjects") %>%
+    add_statistic("N raters", x$raters, "N", "raters") %>%
+    add_statistic(
+      name = x$icc.name, 
+      value = x$value, 
+      interval = "CI", 
+      level = x$conf.level, 
+      lower = x$lbound, 
+      upper = x$ubound
+    ) %>%
+    add_statistic("statistic", x$Fvalue, "F") %>%
+    add_statistic("df numerator", x$df1, "df", "num.") %>%
+    add_statistic("df denominator", x$df2, "df", "den.") %>%
+    add_statistic("p", x$p.value)
   
-  statistics <- add_statistic(statistics, "N subjects", x$subjects, "N", 
-    "subjects")
-  statistics <- add_statistic(statistics, "N raters", x$raters, "N", "raters")
-  statistics <- add_statistic(statistics, "ICC", x$value, interval = "CI", 
-    level = x$conf.level, lower = x$lbound, upper = x$ubound)
-  statistics <- add_statistic(statistics, "statistic", x$Fvalue, "F")
-  statistics <- add_statistic(statistics, "df numerator", x$df1, "df", "num.")
-  statistics <- add_statistic(statistics, "df denominator", x$df2, "df", "den.")
-  statistics <- add_statistic(statistics, "p", x$p.value)
-  
-  # Add statistics to the analysis
   analysis$statistics <- statistics
   
-  # Add additional information
   analysis$model <- x$model
   analysis$type <- x$type
   analysis$unit <- x$unit
-  analysis$ICC_name <- x$icc.name
   
-  alternative <- list(null_value = x$r0)
-  analysis$alernative <- alternative
+  analysis$alernative <- list(null_value = x$r0)
   
-  # Add package information
   analysis <- add_package_info(analysis, "irr")
   
   return(analysis)
---
+++ tests/testthat/test_irr.R
@@ -0,0 +1,37 @@
+
+# Setup -------------------------------------------------------------------
+
+# Load packages
+library(irr)
+
+# Load test data
+path <- system.file("tests/data/irr.json", package = "tidystats")
+expected_statistics <- read_stats(path)
+
+# aov() -------------------------------------------------------------------
+
+test_that("irr's icc twoway agreement works", {
+  data(anxiety)
+  
+  model <-icc(anxiety, model = "twoway", type = "agreement")
+  
+  expect_equal_models(
+    model = model, 
+    expected_tidy_model = expected_statistics$ICC_anxiety
+  )
+})
+
+test_that("irrs's icc twoway consistency works", {
+  set.seed(1)
+  
+  r1 <- round(rnorm(20, 10, 4))
+  r2 <- round(r1 + 10 + rnorm(20, 0, 2))
+  r3 <- round(r1 + 20 + rnorm(20, 0, 2))
+  
+  model <- icc(cbind(r1, r2, r3), "twoway")
+  
+  expect_equal_models(
+    model = model, 
+    expected_tidy_model = expected_statistics$ICC_high_consistency
+  )
+})
